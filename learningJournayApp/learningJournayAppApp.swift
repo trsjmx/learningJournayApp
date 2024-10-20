6e61b5b68c30b61c8f3d75b0e0f88cc3c33e44cb
trsjmx <tawfiq1113@gmail.com>
2024-10-20 15:31:55 +0300

    Initial Commit

---
+++ learningJournayApp/learningJournayAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  learningJournayAppApp.swift
+//  learningJournayApp
+//
+//  Created by Eng.Arwa on 17/04/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct learningJournayAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ learningJournayAppTests/learningJournayAppTests.swift
@@ -0,0 +1,16 @@
+//
+//  learningJournayAppTests.swift
+//  learningJournayAppTests
+//
+//  Created by Eng.Arwa on 17/04/1446 AH.
+//
+
+import Testing
+
+struct learningJournayAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
